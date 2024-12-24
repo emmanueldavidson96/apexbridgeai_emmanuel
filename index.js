4968a2a7b2841b36a59a12c8e5f3169035567c86
Emmanuel <emmanueldavidson524@gmail.com>
2024-12-24 16:19:56 +0100

    Updated

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: "https://www.apexbridge.ai/",
     credentials: true
 }))
 
