b1c2d86b9d37534ba54d1865297a0debcd4f1575
Emmanuel <emmanueldavidson524@gmail.com>
2024-12-24 17:41:26 +0100

    Yes

--- Utils/generateVerificationTokenAndSetCookie.js
+++ Utils/generateVerificationTokenAndSetCookie.js
@@ -7,8 +7,8 @@ const generateTokenAndSetCookie = (response, userId) => {
 
     response.cookie("apexBridgeToken", token, {
         httpOnly:true,
-        secure: process.env.NODE_ENV === "production",
-        sameSite: process.env.NODE_ENV === "production" ? "none" :"lax",
+        secure: true,
+        sameSite: "none",
         maxAge: 7 * 24 * 60 * 60 * 1000,
     })
 
