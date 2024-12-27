6174c0df9a38d4d93645c3e4c45b6b4058d48c5d
Emmanuel <emmanueldavidson524@gmail.com>
2024-12-27 16:25:06 +0100

    Updated all

--- Mail/Mailtrap.config.js
+++ Mail/Mailtrap.config.js
@@ -1,6 +1,34 @@
 const {MailtrapClient} = require("mailtrap");
 const dotenv = require("dotenv");
 
+// const { MailtrapClient } = require("mailtrap");
+
+// const TOKEN = "9600a1104b94c84b00882c2a08fb7d33";
+
+// const client = new MailtrapClient({
+//   token: TOKEN,
+// });
+
+// const sender = {
+//   email: "hello@apexbridge.ai",
+//   name: "Mailtrap Test",
+// };
+// const recipients = [
+//   {
+//     email: "support@apexbridge.ai",
+//   }
+// ];
+
+// client
+//   .send({
+//     from: sender,
+//     to: recipients,
+//     subject: "You are awesome!",
+//     text: "Congrats for sending test email with Mailtrap!",
+//     category: "Integration Test",
+//   })
+//   .then(console.log, console.error);
+
 dotenv.config();
 
 const TOKEN = process.env.MAILTRAP_TOKEN;
@@ -12,7 +40,7 @@ const mailtrapClient = new MailtrapClient({
 })
 
 const sender = {
-    email: "hello@demomailtrap.com",
+    email: "hello@apexbridge.ai",
     name: "Apex Bridge AI"
 }
 
