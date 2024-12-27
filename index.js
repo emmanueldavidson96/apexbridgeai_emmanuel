3626da22065d443b578f46a8474f5803b1f21b13
Emmanuel <emmanueldavidson524@gmail.com>
2024-12-27 21:05:51 +0100

    Updated the api

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json())
 app.use(cookieParser())
 
 app.use(cors({
-    origin: "https://www.apexbridge.ai",
+    origin: ["https://www.apexbridge.ai", "https://www.apexbridge.ai/register"],
     credentials: true,
 }))
 
